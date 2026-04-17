7b4336aa101e5f726282d6a62a07b11dc84b73e1
Mykyta Kuzminov <mykytakuzminov@gmail.com>
2026-04-17 16:13:36 +0200

    feat(lab02): implement SparseVector class with constructors, destructor, << and >> operators

---
+++ lab02/main.cpp
@@ -0,0 +1,116 @@
+#include <iostream>
+
+using namespace std;
+
+class SparseVector {
+private:
+    struct Node {
+        int index;
+        double value;
+        Node* next;
+
+        Node(int index, double value) {
+            this->index = index;
+            this->value = value;
+            this->next = nullptr;
+        }
+    };
+
+    int dimension;
+    Node* head;
+
+public:
+    SparseVector() {
+        this->dimension = 0;
+        this->head = nullptr;
+    };
+
+    SparseVector(int dimension) {
+        this->dimension = dimension;
+        this->head = nullptr;
+    }
+
+    SparseVector(int dimension, double* arr) {
+        this->dimension = dimension;
+
+        for (int i = 0; i < dimension; i++) {
+            this->Append(i, arr[i]);
+        }
+    }
+
+    ~SparseVector() {
+        this->Clear();
+    };
+
+    friend ostream& operator<<(ostream& os, const SparseVector& v) {
+        Node* current = v.head;
+        os << "[";
+        for (int i = 0; i < v.dimension; i++) {
+            if (current != nullptr && current->index == i) {
+                os << current->value;
+                current = current->next;
+            } else {
+                os << 0;
+            }
+
+            if (i < v.dimension - 1) os << ", ";
+        }
+        os << "]" << endl;
+        return os;
+    }
+
+    friend istream& operator>>(istream& is, SparseVector& v) {
+        v.Clear();
+
+        cout << "Enter " << v.dimension << "  coordinates: ";
+        for (int i = 0; i < v.dimension; i++) {
+            double value;
+            is >> value;
+            v.Append(i, value);
+        }
+        return is;
+    }
+
+private:
+    void Append(int index, double value) {
+        if (value == 0) return;
+
+        Node* newNode = new Node(index, value);
+
+        if (this->head == nullptr) {
+            this->head = newNode;
+            return;
+        }
+
+        Node* current = this->head;
+        while(current->next) {
+            current = current->next;
+        }
+        current->next = newNode;
+        return;
+    }
+
+    void Clear() {
+        while(head) {
+            Node* temp = head;
+            head = head->next;
+            delete temp;
+        }
+    }
+};
+
+int main() {
+    int dimension = 5;
+    double* arr = new double[dimension]{1.0, 0.0, 0.0, 2.0, 0.0};
+
+    SparseVector* vector = new SparseVector(dimension, arr);
+
+    cout << *vector;
+    cin >> *vector;
+    cout << *vector;
+
+    delete vector;
+    delete[] arr;
+
+    return 0;
+}
